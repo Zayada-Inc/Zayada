a4cff48186337863f91b88600b9cbc4e07f11c68
Mihnea Zotica <zoticam@nala.roche.com>
2023-02-27 23:33:32 +0200

    Added Repo Pattern and Specification Pattern

---
+++ Domain/Interfaces/IGenericRepository.cs
@@ -0,0 +1,15 @@
+﻿using Domain.Entities;
+
+namespace Domain.Interfaces
+{
+    public interface IGenericRepository<T> where T : EntityBase
+    {
+        Task<T> AddAsync(T entity);
+        Task<int> CountAsync(ISpecification<T> spec);
+        Task<int> DeleteAsync(ISpecification<T> spec);
+        Task<T> GetByIdAsync(int id);
+        Task<T> GetEntityWithSpec(ISpecification<T> spec);
+        Task<IReadOnlyList<T>> ListAllAsync();
+        Task<IReadOnlyList<T>> ListAsync(ISpecification<T> spec);
+    }
+}
---
+++ Domain/Specifications/PersonalTrainers/PersonalTrainersSpecification.cs
@@ -0,0 +1,49 @@
+﻿using Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Specifications.PersonalTrainers
+{
+    public class PersonalTrainersSpecification : BaseSpecification<PersonalTrainer>
+    {
+        public PersonalTrainersSpecification() { 
+        AddInclude(x => x.Gym);
+        }
+        public PersonalTrainersSpecification(Guid id): base(x => x.Id == id)
+        {
+        }
+
+        public PersonalTrainersSpecification(PersonalTrainersParam personalTrainersParam): base(x =>
+                                  (string.IsNullOrEmpty(personalTrainersParam.Search) || x.Name.ToLower().Contains(personalTrainersParam.Search)) &&
+                                  (!personalTrainersParam.GymId.HasValue || x.GymId == personalTrainersParam.GymId)
+                              )
+        {
+            AddInclude(x => x.Gym);
+            ApplyPaging(personalTrainersParam.PageSize * (personalTrainersParam.PageIndex - 1), personalTrainersParam.PageSize);
+            AddOrderBy(n => n.Name.ToLower());
+            if (!string.IsNullOrEmpty(personalTrainersParam.Sort))
+            {
+                switch (personalTrainersParam.Sort)
+                {/*
+                    case "priceAsc":
+                        AddOrderBy(p => p.Price);
+                        break;
+                    case "priceDesc":
+                        AddOrderByDescending(p => p.Price);
+                        break;
+                    case "idAsc":
+                        AddOrderBy(p => p.Id);
+                        break;
+                    */
+                    default:
+                        AddOrderBy(n => n.Name);
+                        break;
+                }
+            }
+        }
+
+    }
+}
