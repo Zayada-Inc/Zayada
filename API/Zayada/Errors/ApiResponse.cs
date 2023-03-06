917c8d3ad2b35f404d0b4ba90f309faff4b1358c
Mihnea Zotica <zoticam@nala.roche.com>
2023-03-07 00:37:08 +0200

    added Error Handling

---
+++ API/Zayada/Errors/ApiResponse.cs
@@ -0,0 +1,27 @@
+﻿namespace ZayadaAPI.Errors
+{
+    public class ApiResponse
+    {
+        public ApiResponse(int statusCode, string message = null)
+        {
+            StatusCode = statusCode;
+            Message = message ?? GetDefaultMessageForStatusCode(statusCode);
+
+        }
+
+        public int StatusCode { get; set; }
+        public string Message { get; set; }
+
+        private string GetDefaultMessageForStatusCode(int statusCode)
+        {
+            return statusCode switch
+            {
+                400 => "Ai facut o greseala fratele meu",
+                401 => "N-ai voie, du-te de aici",
+                404 => "N-am gasit fratele meu nimic",
+                500 => "Sunt si erori care traiesc cat o capodopera.",
+                _ => null
+            };
+        }
+    }
+}
---
+++ API/Zayada/MiddleWare/ExceptionMiddleware.cs
@@ -0,0 +1,44 @@
+﻿using System.Net;
+using System.Text.Json;
+using ZayadaAPI.Errors;
+
+namespace ZayadaAPI.MiddleWare
+{
+    public class ExceptionMiddleware
+    {
+        private readonly RequestDelegate _next;
+        private readonly ILogger<ExceptionMiddleware> _logger;
+        private readonly IHostEnvironment _env;
+
+        public ExceptionMiddleware(RequestDelegate next, ILogger<ExceptionMiddleware> logger, IHostEnvironment env)
+        {
+            _next = next;
+            _logger = logger;
+            _env = env;
+        }
+
+        public async Task InvokeAsync(HttpContext context)
+        {
+            try
+            {
+                await _next(context);
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex, ex.Message);
+                context.Response.ContentType = "application/json";
+                context.Response.StatusCode = (int) HttpStatusCode.InternalServerError;
+
+                var response = _env.IsDevelopment()
+                    ? new ApiException((int)HttpStatusCode.InternalServerError, ex.Message, ex.StackTrace.ToString())
+                    : new ApiException((int)HttpStatusCode.InternalServerError);
+
+                var options = new JsonSerializerOptions { PropertyNamingPolicy = JsonNamingPolicy.CamelCase };
+
+                var json = JsonSerializer.Serialize(response, options);
+
+                await context.Response.WriteAsync(json);
+            }
+        }
+    }
+}
