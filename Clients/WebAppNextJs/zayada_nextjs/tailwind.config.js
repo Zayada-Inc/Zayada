4913fe4241e95708cf7517d2fb99344567fc8329
Mihnea Zotica <zoticam@nala.roche.com>
2023-03-14 00:30:12 +0200

    fixed flowbite issues

--- Clients/WebAppNextJs/zayada_nextjs/tailwind.config.js
+++ Clients/WebAppNextJs/zayada_nextjs/tailwind.config.js
@@ -1,8 +1,12 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{js,ts,jsx,tsx}"],
+  content: [
+    "./src/**/*.{js,ts,jsx,tsx,html}",
+    "./node_modules/flowbite/**/*.{js,ts,jsx,tsx,html}",
+    "./node_modules/flowbite-react/**/*.{js,ts,jsx,tsx,html}"
+  ],
   theme: {
     extend: {},
   },
-  plugins: [],
-}
+  plugins: [require("flowbite/plugin")]
+};
