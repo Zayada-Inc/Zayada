ecb7bb9e8fc13666ea4606143dc454b6583849e8
mihneazotica <mihneazotica@gmail.com>
2023-04-04 21:28:46 +0300

    test3

--- Clients/WebAppNextJs/zayada_nextjs/next.config.js
+++ Clients/WebAppNextJs/zayada_nextjs/next.config.js
@@ -1,7 +1,11 @@
 /** @type {import('next').NextConfig} */
+
+const path = require('path');
 const nextConfig = {
   experimental: {
     appDir: true,
+    reactStrictMode: true,
+    pagesDirectory: path.join(__dirname, 'src/app'),
   },
 }
 
