162bf2e30b1cda0c48bb514e40a900056c2dafa9
Mihnea Zotica <zoticam@nala.roche.com>
2023-04-17 22:36:07 +0300

    Added specification for gym

--- API/Zayada/Controllers/GymController.cs
+++ API/Zayada/Controllers/GymController.cs
@@ -6,6 +6,8 @@ using Domain.Entities.IdentityEntities;
 using Application.CommandsQueries.Gyms;
 using Application.Helpers;
 using Application.CommandsQueries.GymSubscriptionPlan;
+using Domain.Specifications.Gyms;
+using Domain.Helpers;
 
 namespace ZayadaAPI.Controllers
 {
@@ -14,14 +16,14 @@ namespace ZayadaAPI.Controllers
     {
         [Cached(30)]
         [HttpGet]
-        public async Task<ActionResult<IReadOnlyList<GymsToReturnDto>>> GetGyms()
+        public async Task<ActionResult<Pagination<GymsToReturnDto>>> GetGyms([FromQuery]GymsParam gymsParam)
         {
-            var gyms = await Mediator.Send(new GymsList.Query());
-            if (gyms.Count == 0)
+            var gyms = await Mediator.Send(new GymsList.Query { GymParams = gymsParam});
+            
+            if (gyms.Data.Count == 0)
             {
                 return NotFound(new ApiResponse(404));
             }
-
             return Ok(gyms);
         }
 
