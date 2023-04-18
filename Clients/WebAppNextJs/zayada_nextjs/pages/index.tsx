06afbdaf525e435c769e2708a6c92cb96f0b7efa
Mihnea Zotica <zoticam@nala.roche.com>
2023-04-19 00:01:38 +0300

    fetch data from api

--- Clients/WebAppNextJs/zayada_nextjs/pages/index.tsx
+++ Clients/WebAppNextJs/zayada_nextjs/pages/index.tsx
@@ -2,7 +2,21 @@
 import { Navbar, Button } from "flowbite-react";
 import RootLayout from "./layout";
 
-export default function Home() {
+interface HomeProps {
+  homeString: string;
+}
+
+export async function getStaticProps() {
+  try {
+  const res = await fetch("http://localhost:5001/");
+  const data = await res.text();
+  return {props: {homeString: data}};
+  } catch (error) {
+    return {props: {homeString: "NaN"}};
+  }
+}
+
+const Home: React.FC<HomeProps> = ({homeString}) => {
   return (
     <RootLayout>
     <main className=" min-h-screen w-screen bg-slate-200">
@@ -47,13 +61,39 @@ export default function Home() {
         <div className="container mx-auto">
           <div className="flex flex-col items-center justify-center min-h-screen py-2">
             <h1 className="text-4xl font-bold text-gray-800 dark:text-white text-center border-spacing-1 ">
-              Hello DuDu! :)
+              {homeString} :)
             </h1>
           </div>
         </div>
       </main>
     </main>
+    <footer className="bg-white rounded-lg shadow dark:bg-gray-900 m-4">
+    <div className="w-full max-w-screen-xl mx-auto p-4 md:py-8">
+        <div className="sm:flex sm:items-center sm:justify-between">
+            <a href="#" className="flex items-center mb-4 sm:mb-0">
+                <img src="https://res.cloudinary.com/dgwkfkpve/image/upload/v1680804744/ozsrf72ko2ia9obn40iz.png" className="h-72 w-72" alt="Flowbite Logo" />
+            </a>
+            <ul className="flex flex-wrap items-center mb-6 text-sm font-medium text-gray-500 sm:mb-0 dark:text-gray-400">
+                <li>
+                    <a href="#" className="mr-4 hover:underline md:mr-6 ">About</a>
+                </li>
+                <li>
+                    <a href="#" className="mr-4 hover:underline md:mr-6">Privacy Policy</a>
+                </li>
+                <li>
+                    <a href="#" className="mr-4 hover:underline md:mr-6 ">Licensing</a>
+                </li>
+                <li>
+                    <a href="#" className="hover:underline">Contact</a>
+                </li>
+            </ul>
+        </div>
+        <hr className="my-6 border-gray-200 sm:mx-auto dark:border-gray-700 lg:my-8" />
+        <span className="block text-sm text-gray-500 sm:text-center dark:text-gray-400">© 2023 <a href="https://ZAYADA.com/" className="hover:underline">ZAYADA™</a>. All Rights Reserved.</span>
+    </div>
+</footer>
     </RootLayout>
-  );
+  )
+};
 
-}
+export default Home;
