94014a80af61a83b3d1a667120f2eb443fb7eada
Dando54 <dandoczi.denis@gmail.com>
2023-04-30 21:32:51 +0300

    Personal trainers & Gyms tables

--- Clients/frontend/src/store/slices/search.ts
+++ Clients/frontend/src/store/slices/search.ts
@@ -1,18 +1,30 @@
 import { createSlice, PayloadAction } from '@reduxjs/toolkit';
 import { RootState } from 'store/store';
 
+export type queryPage = {
+  query: string;
+  activePage: number;
+};
+
 type SearchState = {
   tables: {
-    allUsers: {
-      query: string;
-      activePage: number;
-    };
+    allUsers: queryPage;
+    gym: queryPage;
+    pt: queryPage;
   };
 };
 
 const initialState: SearchState = {
   tables: {
     allUsers: {
+      query: '',
+      activePage: 1,
+    },
+    gym: {
+      query: '',
+      activePage: 1,
+    },
+    pt: {
       query: '',
       activePage: 1,
     },
@@ -29,9 +41,30 @@ export const searchSlice = createSlice({
     setAllUsersPage: (state, action: PayloadAction<number>) => {
       state.tables.allUsers.activePage = action.payload;
     },
+    setGymSearch: (state, action: PayloadAction<string>) => {
+      state.tables.gym.query = action.payload;
+    },
+    setGymPage: (state, action: PayloadAction<number>) => {
+      state.tables.gym.activePage = action.payload;
+    },
+    setPtSearch: (state, action: PayloadAction<string>) => {
+      state.tables.pt.query = action.payload;
+    },
+    setPtPage: (state, action: PayloadAction<number>) => {
+      state.tables.pt.activePage = action.payload;
+    },
   },
 });
 
 export const getAllUsers = (state: RootState) => state.search.tables.allUsers;
+export const getAllGym = (state: RootState) => state.search.tables.gym;
+export const getAllPt = (state: RootState) => state.search.tables.pt;
 
-export const { setAllUsersSearch, setAllUsersPage } = searchSlice.actions;
+export const {
+  setAllUsersSearch,
+  setAllUsersPage,
+  setGymPage,
+  setGymSearch,
+  setPtPage,
+  setPtSearch,
+} = searchSlice.actions;
